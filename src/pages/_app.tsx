8e9aeffc490fac637fbac83bad24ab9fbe571bba
Niels Bik <nielsrowinbik@gmail.com>
2021-05-23 15:09:51 +0200

    Add TailwindCSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,62 +4,10 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { DefaultSeo } from "next-seo";
 import { useEffect } from "react";
-import { createGlobalStyle } from "styled-components";
 
 import { ProvideBrewsContext } from "../hooks/use-brews-context";
 
-const GlobalStyle = createGlobalStyle`
-    html, time {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-
-    html {
-        background-color: #ff1744;
-        color: #ffffff;
-    }
-
-    body {
-        margin: 0;
-    }
-
-    *,
-    *:before,
-    *:after {
-        box-sizing: border-box;
-    }
-
-    h1 {
-        font-family: Montserrat;
-        font-size: 5rem;
-        font-weight: 700;
-    }
-
-    h2 {
-        font-family: Montserrat;
-        font-size: 3rem;
-        font-weight: 600;
-
-    }
-
-    h3 {
-        font-size: 1.8rem;
-        font-weight: 600;
-    }
-
-    h4 {
-        font-family: Montserrat;
-        font-size: 1.3rem;
-        font-weight: 600;
-    }
-
-    #__next {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 64px 1fr 64px;
-        grid-template-areas: "nav" "main" "footer";
-        height: 100%;
-    }
-`;
+import "../styles/globals.css";
 
 const updateHeight = () => {
     document.body.style.height = `${window.innerHeight}px`;
@@ -79,7 +27,6 @@ const App = ({ Component, pageProps }: AppProps) => {
 
     return (
         <>
-            <GlobalStyle />
             <DefaultSeo
                 titleTemplate="%s | Brewtime"
                 title="Create the perfect cup of coffee"
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const { trueGray } = require("tailwindcss/colors");
+const { colors } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    darkMode: "class",
+    mode: "jit",
+    plugins: [],
+    purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx"],
+    theme: {
+        colors: {
+            ...colors,
+            gray: trueGray,
+        },
+    },
+    variants: {},
+};
