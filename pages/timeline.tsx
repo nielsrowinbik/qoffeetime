be8bd3fc3bc6b361684232ff758a59e9e66df361
Niels Bik <nielsrowinbik@gmail.com>
2021-06-01 20:25:19 +0200

    Begin work on timeline view

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,3 +1,25 @@
-const Timeline = () => <h1>timeline</h1>;
+import { mdiClose, mdiCoffeeOutline } from '@mdi/js';
+import Link from 'next/link';
+
+import FooterLayout from '../layouts/FooterLayout';
+import MainLayout from '../layouts/MainLayout';
+import NavLayout from '../layouts/NavLayout';
+
+import Button from '../components/Button';
+import IconButton from '../components/IconButton';
+
+const Timeline = () => (
+    <>
+        <NavLayout>
+            <Link href="/">
+                <IconButton icon={mdiClose} small />
+            </Link>
+        </NavLayout>
+        <MainLayout>todo!</MainLayout>
+        <FooterLayout>
+            <Button icon={mdiCoffeeOutline}>Add a brew</Button>
+        </FooterLayout>
+    </>
+);
 
 export default Timeline;
