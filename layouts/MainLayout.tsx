0bc0e74231462bfa3bf22212fe353908c39c4e7e
Niels Bik <nielsrowinbik@gmail.com>
2021-06-01 20:25:00 +0200

    Change scrolling behaviour

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,6 +1,9 @@
 export default function MainLayout({ children }) {
     return (
-        <main className="px-2" style={{ gridArea: 'main' }}>
+        <main
+            className="flex flex-col px-6 overflow-hidden"
+            style={{ gridArea: 'main' }}
+        >
             {children}
         </main>
     );
