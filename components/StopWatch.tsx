00d50630bc7993366268aef21026b97ca399efc4
Niels Bik <nielsrowinbik@gmail.com>
2021-06-04 17:22:41 +0200

    Extract StopWatch out to its own component

---
+++ components/StopWatch.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import type { PropsWithChildren } from 'react';
+
+type StopwatchProps = {
+    children: string;
+    className?: string;
+};
+
+const StopWatch = ({ children, ...props }: StopwatchProps) => {
+    const className = classNames(
+        'relative border border-white rounded-full w-8 h-8 text-xs flex items-center justify-center flex-none font-semibold',
+        props.className
+    );
+
+    return <time className={className}>{children}</time>;
+};
+
+export default StopWatch;
