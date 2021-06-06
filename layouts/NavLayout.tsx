84659a9618263e84d71a9f041a840793be98a04b
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 14:25:51 +0200

    Make children optional in NavLayout

--- layouts/NavLayout.tsx
+++ layouts/NavLayout.tsx
@@ -1,10 +1,14 @@
-export default function NavLayout({ children }) {
-    return (
-        <nav
-            className="flex flex-row items-center px-2"
-            style={{ gridArea: 'nav' }}
-        >
-            {children}
-        </nav>
-    );
-}
+import type { PropsWithChildren } from 'react';
+
+type NavLayoutProps = PropsWithChildren<{}>;
+
+const NavLayout = ({ children }: NavLayoutProps) => (
+    <nav
+        className="flex flex-row items-center px-2"
+        style={{ gridArea: 'nav' }}
+    >
+        {children}
+    </nav>
+);
+
+export default NavLayout;
