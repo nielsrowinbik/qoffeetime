45824e7851a1363f988fc9ae05a7cba466f4355f
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 15:35:51 +0200

    Add inGroup prop to Button, add automatically when in group

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,27 @@
 import classNames from 'classnames';
-import { forwardRef } from 'react';
+import { Children, cloneElement, forwardRef, isValidElement } from 'react';
+import type { HTMLProps, PropsWithChildren, ReactElement } from 'react';
 import Icon from '@mdi/react';
-
-import type { HTMLProps, PropsWithChildren } from 'react';
 
 type ButtonProps = PropsWithChildren<
     HTMLProps<HTMLAnchorElement> &
         HTMLProps<HTMLButtonElement> & {
             href?: string;
             icon?: string;
+            inGroup?: boolean;
             type?: 'button' | 'submit' | 'reset';
             variant?: 'dark' | 'light' | 'text';
         }
 >;
 
 const Button = forwardRef<HTMLAnchorElement & HTMLButtonElement, ButtonProps>(
-    ({ children, variant = 'light', ...props }, ref) => {
+    ({ children, inGroup = false, variant = 'light', ...props }, ref) => {
         const className = classNames(
-            'font-semibold w-full py-3 first:rounded-l-lg first:rounded-r-none last:rounded-r-lg last:rounded-l-none only:rounded-lg inline-flex justify-center align-center disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed relative overflow-hidden outline-none',
+            'font-semibold w-full py-3 inline-flex justify-center align-center disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed relative overflow-hidden outline-none',
             {
+                'rounded-lg': !inGroup,
+                'first:rounded-l-lg first:rounded-r-none last:rounded-r-lg last:rounded-l-none only:rounded-lg':
+                    inGroup,
                 'bg-white text-black': variant === 'light',
                 'bg-black text-white': variant === 'dark',
                 'text-current': variant === 'text',
@@ -40,7 +43,13 @@ const Button = forwardRef<HTMLAnchorElement & HTMLButtonElement, ButtonProps>(
 type ButtonGroupProps = PropsWithChildren<{}>;
 
 const ButtonGroup = ({ children }: ButtonGroupProps) => (
-    <div className="flex flex-row">{children}</div>
+    <div className="flex flex-row">
+        {Children.map(children, (child) => {
+            if (isValidElement(child))
+                return cloneElement(child, { inGroup: true });
+            return child;
+        })}
+    </div>
 );
 
 export default Button;
