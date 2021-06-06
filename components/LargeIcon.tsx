57e0651b3f394d2f86fd0de50fbb48fcb081a6e1
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 14:09:37 +0200

    Inline LargeIcon

--- components/LargeIcon.tsx
+++ components/LargeIcon.tsx
@@ -5,7 +5,7 @@ type LargeIconProps = {
 };
 
 const LargeIcon = ({ icon }: LargeIconProps) => (
-    <div className="w-16 h-16 rounded-full bg-white bg-opacity-40 flex items-center justify-center">
+    <div className="w-16 h-16 rounded-full bg-white bg-opacity-40 inline-flex items-center justify-center">
         <Icon path={icon} size={1.5} />
     </div>
 );
