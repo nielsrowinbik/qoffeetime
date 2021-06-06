61a49202f5694b1166bf5fe8e1b9d2388808fa22
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 13:57:23 +0200

    Add LargeIcon component

---
+++ components/LargeIcon.tsx
@@ -0,0 +1,13 @@
+import Icon from '@mdi/react';
+
+type LargeIconProps = {
+    icon: string;
+};
+
+const LargeIcon = ({ icon }: LargeIconProps) => (
+    <div className="w-16 h-16 rounded-full bg-white bg-opacity-40 flex items-center justify-center">
+        <Icon path={icon} size={1.5} />
+    </div>
+);
+
+export default LargeIcon;
