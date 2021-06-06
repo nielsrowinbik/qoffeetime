8390077896255085f1c8626ae8a762e8f8cccc95
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 13:57:15 +0200

    Less styling on Footer

--- layouts/FooterLayout.tsx
+++ layouts/FooterLayout.tsx
@@ -1,10 +1,11 @@
-export default function Footer({ children }) {
-    return (
-        <footer
-            className="flex flex-row items-start px-4"
-            style={{ gridArea: 'footer' }}
-        >
-            {children}
-        </footer>
-    );
-}
+import type { PropsWithChildren } from 'react';
+
+type FooterProps = PropsWithChildren<{}>;
+
+const Footer = ({ children }: FooterProps) => (
+    <footer className="px-4" style={{ gridArea: 'footer' }}>
+        {children}
+    </footer>
+);
+
+export default Footer;
