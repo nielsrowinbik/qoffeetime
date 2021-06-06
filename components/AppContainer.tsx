3e1b49e126b4ace874776664736636bc49dbe8ac
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 12:50:02 +0200

    Add pagination bullets

--- components/AppContainer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Container({ children }) {
-    return (
-        <div className="h-screen max-w-screen-sm flex flex-col mx-auto">
-            <div className="overflow-x-hidden overflow-y-auto h-full">
-                {children}
-            </div>
-        </div>
-    );
-}
