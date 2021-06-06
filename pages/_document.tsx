5638d2e4abf6be5107680105ecc2161115e84f0e
Niels Bik <nielsrowinbik@gmail.com>
2021-06-06 17:00:30 +0200

    Update icon set

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,48 +9,8 @@ class MyDocument extends Document {
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                     <link
                         rel="apple-touch-icon"
-                        sizes="57x57"
-                        href="/assets/icons/apple-touch-icon-57x57.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="60x60"
-                        href="/assets/icons/apple-touch-icon-60x60.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="72x72"
-                        href="/assets/icons/apple-touch-icon-72x72.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="76x76"
-                        href="/assets/icons/apple-touch-icon-76x76.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="114x114"
-                        href="/assets/icons/apple-touch-icon-114x114.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="120x120"
-                        href="/assets/icons/apple-touch-icon-120x120.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="144x144"
-                        href="/assets/icons/apple-touch-icon-144x144.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="152x152"
-                        href="/assets/icons/apple-touch-icon-152x152.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
                         sizes="180x180"
-                        href="/assets/icons/apple-touch-icon-180x180.png"
+                        href="/assets/icons/apple-touch-icon.png"
                     />
                     <link
                         rel="icon"
@@ -61,35 +21,26 @@ class MyDocument extends Document {
                     <link
                         rel="icon"
                         type="image/png"
-                        sizes="194x194"
-                        href="/assets/icons/favicon-194x194.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
-                        sizes="192x192"
-                        href="/assets/icons/android-chrome-192x192.png"
-                    />
-                    <link
-                        rel="icon"
-                        type="image/png"
                         sizes="16x16"
                         href="/assets/icons/favicon-16x16.png"
                     />
-                    <link rel="manifest" href="/site.webmanifest" />
                     <link
                         rel="mask-icon"
                         href="/assets/icons/safari-pinned-tab.svg"
-                        color="#ffffff"
+                        color="#ff1744"
+                    />
+                    <link
+                        rel="shortcut icon"
+                        href="/assets/icons/favicon.ico"
+                    />
+                    <meta name="msapplication-TileColor" content="#ff1744" />
+                    <meta
+                        name="msapplication-config"
+                        content="/browserconfig.xml"
                     />
                     <meta
                         name="description"
                         content="Create the perfect cup of coffee using one of Brewtime's many built-in recipies."
-                    />
-                    <meta name="msapplication-TileColor" content="#383c43" />
-                    <meta
-                        name="msapplication-TileImage"
-                        content="/assets/icons/mstile-144x144.png"
                     />
                     <meta name="theme-color" content="#ff1744" />
                 </Head>
