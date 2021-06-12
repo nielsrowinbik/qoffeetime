d226aa1582e8b3a2d1ca980e3227e768b8db2963
Niels Bik <nielsrowinbik@gmail.com>
2021-06-12 13:26:56 +0200

    Allow user to add additional classes to NavLayout

--- layouts/NavLayout.tsx
+++ layouts/NavLayout.tsx
@@ -1,14 +1,21 @@
+import classNames from 'classnames';
 import type { PropsWithChildren } from 'react';
 
-type NavLayoutProps = PropsWithChildren<{}>;
+type NavLayoutProps = PropsWithChildren<{
+    className?: string;
+}>;
 
-const NavLayout = ({ children }: NavLayoutProps) => (
-    <nav
-        className="flex flex-row items-center px-2"
-        style={{ gridArea: 'nav' }}
-    >
-        {children}
-    </nav>
-);
+const NavLayout = ({ children, ...props }: NavLayoutProps) => {
+    const className = classNames(
+        'flex flex-row items-center px-2',
+        props.className
+    );
+
+    return (
+        <nav className={className} style={{ gridArea: 'nav' }}>
+            {children}
+        </nav>
+    );
+};
 
 export default NavLayout;
