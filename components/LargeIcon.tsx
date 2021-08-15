5c3b1145df0dfc8eab517ef3efb846ca011e95cf
Niels Bik <nielsrowinbik@gmail.com>
2021-08-15 17:05:08 +0200

    Refactor LargeIcon to allow for props

--- components/LargeIcon.tsx
+++ components/LargeIcon.tsx
@@ -1,13 +1,22 @@
+import classNames from 'classnames';
+import type { HTMLProps } from 'react';
 import Icon from '@mdi/react';
 
-type LargeIconProps = {
+type LargeIconProps = HTMLProps<HTMLDivElement> & {
     icon: string;
 };
 
-const LargeIcon = ({ icon }: LargeIconProps) => (
-    <div className="w-20 h-20 rounded-full bg-white bg-opacity-40 inline-flex items-center justify-center">
-        <Icon path={icon} size={2} />
-    </div>
-);
+const LargeIcon = ({ icon, ...props }: LargeIconProps) => {
+    const className = classNames(
+        'w-20 h-20 rounded-full bg-white bg-opacity-40 inline-flex items-center justify-center',
+        props.className
+    );
+
+    return (
+        <div {...props} className={className}>
+            <Icon path={icon} size={2} />
+        </div>
+    );
+};
 
 export default LargeIcon;
