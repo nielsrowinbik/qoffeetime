5e7d63c73b81d68979f4a80d24db6d30f7fc3136
Niels Bik <nielsrowinbik@gmail.com>
2021-08-19 16:33:43 +0200

    Allow for classes to be passed to IconButton

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -6,6 +6,7 @@ import type { HTMLProps, PropsWithChildren } from 'react';
 
 type IconButtonProps = HTMLProps<HTMLAnchorElement> &
     HTMLProps<HTMLButtonElement> & {
+        className?: string;
         href?: string;
         icon: string;
         small?: boolean;
@@ -17,16 +18,14 @@ const IconButton = forwardRef<
     IconButtonProps
 >(({ icon, small, ...props }, ref) => {
     const Tag = props.href ? 'a' : 'button';
+    const className = classNames(
+        'rounded-full text-white items-center justify-center bg-white bg-opacity-30',
+        { 'p-1': !!small, 'p-2': !small },
+        props.className
+    );
 
     return (
-        <Tag
-            {...props}
-            className={classNames(
-                'rounded-full rounded-full text-white items-center justify-center bg-black bg-opacity-40',
-                { 'p-1': !!small, 'p-2': !small }
-            )}
-            ref={ref}
-        >
+        <Tag {...props} className={className} ref={ref}>
             <Icon path={icon} size={small ? '20px' : 1} />
         </Tag>
     );
