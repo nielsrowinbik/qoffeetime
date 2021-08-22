1d5a95df137b8f60a597e4f64f20a79d5dfea0c7
Niels Bik <nielsrowinbik@gmail.com>
2021-08-22 15:44:39 +0200

    Move timeline button to own file and increase in size

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,7 @@ import { getAllRecipies } from '../lib/recipies';
 import type { Recipe } from '../lib/types';
 
 import Button from '../components/Button';
-import RecipeSlide from '../components/RecipeSlide';
-
-SwiperCore.use([Pagination]);
+import TimelineButton from '../components/TimelineButton';
 
 const IndexPage: FC<{ recipies: Recipe[] }> = (props) => {
     const { isReady, recipies } = useSortRecipiesByBrews(props.recipies);
@@ -28,12 +26,7 @@ const IndexPage: FC<{ recipies: Recipe[] }> = (props) => {
     return (
         <>
             <FullHeightLayout>
-                <Link href="/timeline">
-                    <a className="absolute bg-brewtime-red shadow-sm rounded-full py-1 px-3 z-10 right-10 top-10 flex flex-row items-center text-sm">
-                        <Icon className="mr-2" path={mdiHistory} size="20px" />
-                        <span>Timeline</span>
-                    </a>
-                </Link>
+                <TimelineButton />
                 <div className="pt-6 h-full">
                     <Swiper
                         centeredSlides
