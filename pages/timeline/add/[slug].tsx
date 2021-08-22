23b7fb37d906450945f0979979d159e4b26a4c98
Niels Bik <nielsrowinbik@gmail.com>
2021-08-22 15:49:19 +0200

    Allow user to add a brew after the fact

---
+++ pages/timeline/add/[slug].tsx
@@ -0,0 +1,129 @@
+import { useRouter } from 'next/router';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
+import type { FC } from 'react';
+
+import FooterLayout from '../../../layouts/FooterLayout';
+import MainLayout from '../../../layouts/MainLayout';
+import NavLayout from '../../../layouts/NavLayout';
+import { useBrews } from '../../../lib/brews';
+import { queryArgToNumber, round } from '../../../lib/helpers';
+import { getRecipeFiles, getRecipeBySlug } from '../../../lib/recipies';
+import type { Recipe } from '../../../lib/types';
+
+import BackButton from '../../../components/BackButton';
+import Button from '../../../components/Button';
+import RatioSlider from '../../../components/RatioSlider';
+
+const AddToTimelinePage: FC<Recipe> = ({
+    defaultRatio,
+    maxVolume,
+    minVolume,
+    name,
+    slug,
+}) => {
+    const router = useRouter();
+    const { coffee: coffeeParam, volume: volumeParam } = router.query;
+    const coffee = queryArgToNumber(coffeeParam);
+    const volume = queryArgToNumber(volumeParam);
+
+    const volumeWithDefault = volume || minVolume;
+    const coffeeWithDefault =
+        coffee || (defaultRatio / 1000) * volumeWithDefault;
+    const ratio = round((coffeeWithDefault / volumeWithDefault) * 1000);
+    const [comment, setComment] = useState('');
+
+    const onVolumeChange = (volume: number) =>
+        router.replace(
+            {
+                pathname: slug,
+                query: {
+                    coffee: round((ratio / 1000) * volume),
+                    volume: volume,
+                },
+            },
+            undefined,
+            { shallow: true }
+        );
+
+    const { createBrew } = useBrews();
+    const saveBrew = () =>
+        toast.promise(
+            createBrew({
+                coffee: coffeeWithDefault,
+                recipe: name,
+                volume: volumeWithDefault,
+                ...(comment !== '' && { comment }),
+            }),
+            {
+                loading: 'Saving your brew...',
+                success: () => {
+                    router.replace('/timeline');
+                    return 'Successfully saved your brew!';
+                },
+                error: (error) => {
+                    console.error(error);
+                    return 'Something went wrong saving your brew';
+                },
+            }
+        );
+
+    // Don't render anything until we've parsed query parameters:
+    if (!router.isReady) return null;
+
+    return (
+        <>
+            <NavLayout>
+                <BackButton />
+            </NavLayout>
+            <MainLayout className="flex flex-col">
+                <h1 className="text-5xl font-bold">{name}</h1>
+                <section className="mt-10 mb-16">
+                    <RatioSlider
+                        max={maxVolume}
+                        min={minVolume}
+                        onChange={onVolumeChange}
+                        ratio={ratio}
+                        value={volumeWithDefault}
+                    />
+                </section>
+                <section>
+                    <textarea
+                        autoFocus={false}
+                        className="bg-white bg-opacity-10 p-3 resize-none placeholder-white placeholder-opacity-80 w-full"
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                        placeholder="How was it?"
+                        rows={4}
+                        tabIndex={-1}
+                    />
+                </section>
+            </MainLayout>
+            <FooterLayout>
+                <Button onClick={saveBrew}>Save {name} brew</Button>
+            </FooterLayout>
+        </>
+    );
+};
+
+const getStaticPaths = async () => {
+    const recipies = await getRecipeFiles();
+
+    return {
+        paths: recipies.map((p) => ({
+            params: {
+                slug: p.replace(/\.json/, ''),
+            },
+        })),
+        fallback: false,
+    };
+};
+
+const getStaticProps = async ({ params }) => {
+    const recipe = await getRecipeBySlug(params.slug);
+
+    return { props: { ...recipe } };
+};
+
+export default AddToTimelinePage;
+export { getStaticPaths, getStaticProps };
