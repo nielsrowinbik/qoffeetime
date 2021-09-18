01b730f7e4c132fd87436a0a0af8350ac0bad2a4
Niels Bik <nielsrowinbik@gmail.com>
2021-09-18 21:04:04 +0200

    Rework RatioSlider

--- components/RatioSlider.tsx
+++ components/RatioSlider.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useBoundingclientrectRef } from 'rooks';
 
 import { round } from '../lib/helpers';
@@ -9,8 +8,9 @@ type RatioSliderProps = {
     borderRadius?: number;
     height?: number;
     maxOutput: number;
-    minOutput: number;
     maxRatio?: number;
+    minFill?: number;
+    minOutput: number;
     minRatio?: number;
     ratio: number;
     width?: number;
@@ -20,60 +20,33 @@ type RatioSliderProps = {
 type DraggingState = '' | 'left' | 'right';
 
 const COFFEE_STEP = 1;
+const COFFEE_MAX_RATIO = 2 / 3;
+const COFFEE_MIN_RATIO = 1 / 3;
 const OUTPUT_STEP = 10;
-
-// TODO: Add a `minFill` prop or setting, setting the minimum height for the white area.
-// This setting should also be taken into account when dragging. This way the user can drag from
-// the top of any area and precisely move it into position.
 
 const RatioSlider = ({
     borderRadius = 64,
     height = 320,
     maxOutput,
-    minOutput,
     maxRatio = 100,
+    minFill = 0.5,
+    minOutput,
     minRatio = 30,
     width = 320,
     ...props
 }: RatioSliderProps) => {
     const router = useRouter();
+
+    // Positioning helper:
     const [svgRef, svgRect] = useBoundingclientrectRef();
 
-    // Helper function to compute the percentage the user has dragged up:
-    const _positionToPercentage = useCallback(
-        (posY: number) => {
-            const minValue = height * (1 / 8);
-            const maxValue = height * (7 / 8);
-            const posYWithinShape = Math.min(
-                Math.max(posY - svgRect.top, minValue),
-                maxValue
-            );
-
-            return (
-                100 -
-                ((posYWithinShape - minValue) * 100) / (maxValue - minValue)
-            );
-        },
-        [height, svgRect]
-    );
-
-    // Helper function to convert a percentage to an output value:
-    const _percentageToOutput = useCallback(
-        (percentage: number) => {
-            const value =
-                (percentage / 100) * (maxOutput - minOutput) + minOutput;
-            return Math.ceil(value / OUTPUT_STEP) * OUTPUT_STEP;
-        },
-        [maxOutput, minOutput]
-    );
-
-    // Helper function to convert a percentage to a ratio value:
-    const _percentageToRatio = useCallback(
-        (percentage: number) => {
-            const value = (percentage / 100) * (maxRatio - minRatio) + minRatio;
-            return Math.ceil(value / COFFEE_STEP) * COFFEE_STEP;
-        },
-        [maxRatio, minRatio]
+    // Helper function to compute position within element:
+    const getPosition = useCallback(
+        (e) => ({
+            x: (e.touches ? e.touches[0] : e).clientX - svgRect.left,
+            y: (e.touches ? e.touches[0] : e).clientY - svgRect.top,
+        }),
+        [svgRect]
     );
 
     // Keep internal value representations:
@@ -86,71 +59,168 @@ const RatioSlider = ({
         setOutput(props.output);
     }, [props.ratio, props.output]);
 
-    // Keep whether we're dragging in state:
-    const [dragging, setDragging] = useState<DraggingState>('');
+    // Keep which side user is ddragging on in state:
+    const [side, setSide] = useState<DraggingState>('');
 
-    // Handle dragging start:
-    const onMouseDown = useCallback(
-        (e) => {
-            e.stopPropagation();
+    // Definitions for clipPath shape:
+    const clipPathPath = [
+        'M', // Move to starting position
+        width * (1 / 4) + borderRadius, // Starting X coordinate, top left corner
+        height * (1 / 8), // Starting Y coordinate, top left corner
+        'H', // Horizontal line
+        width * (3 / 4) - borderRadius, // X coordinate, top right corner
+        'q', // Rounded corner
+        `${borderRadius},0 ${borderRadius},${borderRadius}`, // Top right rounded corner
+        'V', // Vertical line
+        height * (7 / 8) - borderRadius, // Y coordinate, bottom right corner
+        'q', // Rounded corner
+        `0,${borderRadius} -${borderRadius},${borderRadius}`, // Bottom right rounded corner
+        'H', // Horizontal line
+        width * (1 / 4) + borderRadius, // X coordinate, bottom left corner
+        'q', // Rounded corner
+        `${-borderRadius},0 -${borderRadius},-${borderRadius}`, // Bottom left rounded corner
+        'V',
+        height * (1 / 8) + borderRadius, // Ending Y coordinate, top left corner
+        'q', // Rounded corner
+        `0,-${borderRadius} ${borderRadius},-${borderRadius}`, // Top left rounded corner
+        'z', // Shape end
+    ].join(' ');
 
-            const posX = (e.touches ? e.touches[0] : e).clientX;
-            const posY = (e.touches ? e.touches[0] : e).clientY;
+    // Definitions for background shape:
+    const backgroundPath = [
+        'M', // Move to starting position
+        width * (1 / 4), // Starting X coordinate, top left corner
+        height * (1 / 8), // Starting Y coordinate, top left corner
+        'H', // Horizontal line
+        width * (3 / 4), // X coordinate, top right corner
+        'V', // Vertical line
+        height * (7 / 8), // Y coordinate, bottom right corner
+        'H', // Horizontal line
+        width * (1 / 4), // X coordinate, bottom left corner
+        'z', // Shape end
+    ];
 
-            // Update state to reflect dragging state (prefer dragging right):
-            const side = posX >= width / 2 ? 'right' : 'left';
-            setDragging(side);
+    // Definitions for output shape:
+    const outputProgress = rangeProgressFromValue(output, minOutput, maxOutput);
+    const outputRange = [(1 - minFill) * height, height * (1 / 8)];
+    const outputPath = [
+        'M', // Move to starting position
+        width * (1 / 4), // Starting X coordinate, top left corner
+        outputRange[0] - outputProgress * (outputRange[0] - outputRange[1]), // Starting Y coordinate, top left corner
+        'H', // Horizontal line
+        width * (3 / 4), // X coordinate, top right corner
+        'V', // Vertical line
+        height * (7 / 8), // Y coordinate, bottom right corner
+        'H', // Horizontal line
+        width * (1 / 4), // X coordinate, bottom left corner
+        'z', // Shape end
+    ];
 
-            // Update internal values:
-            _onChange(posY, side);
+    // Definitions for coffee shape:
+    const coffeeProgress = rangeProgressFromValue(ratio, minRatio, maxRatio);
+    const outputHeight = (outputPath[6] as number) - (outputPath[2] as number);
+    const coffeeRange = [
+        (outputPath[2] as number) + (1 - COFFEE_MIN_RATIO) * outputHeight,
+        (outputPath[2] as number) + (1 - COFFEE_MAX_RATIO) * outputHeight,
+    ];
+    const coffeePath = [
+        'M', // Move to starting position
+        width * (1 / 4), // Starting X coordinate, top left corner
+        round(
+            coffeeRange[0] - coffeeProgress * (coffeeRange[0] - coffeeRange[1])
+        ), // Starting Y coordinate, top left corner
+        'H', // Horizontal line
+        width * (3 / 4), // X coordinate, top right corner
+        'V', // Vertical line
+        height * (7 / 8), // Y coordinate, bottom right corner
+        'H', // Horizontal line
+        width * (1 / 4), // X coordinate, bottom left corner
+        'z', // Shape end
+    ];
+
+    // Definitions for output label:
+    const visibleOutputHeight =
+        (coffeePath[2] as number) - (outputPath[2] as number);
+    const outputLabelPosition = {
+        dx: -10,
+        x: width * (1 / 4),
+        y: (outputPath[2] as number) + visibleOutputHeight / 2,
+    };
+
+    // Definitions for cofee label:
+    const coffeeHeight = (coffeePath[6] as number) - (coffeePath[2] as number);
+    const coffeeLabelPosition = {
+        dx: -10,
+        x: width * (1 / 4),
+        y: (coffeePath[2] as number) + coffeeHeight / 2,
+    };
+
+    // Returns a new output value given a position:
+    const _positionToOutput = useCallback(
+        (posY: number) => {
+            const lowestYValue = outputRange[1]; // Corresponds with highest physical position!
+            const highestYValue = outputRange[0]; // Corresponds with lowset physical position!
+            const posWithinRange = withinRange(
+                posY,
+                lowestYValue,
+                highestYValue
+            );
+            const percentage =
+                1 -
+                rangeProgressFromValue(
+                    posWithinRange,
+                    lowestYValue,
+                    highestYValue
+                );
+            const value = rangeValueFromProgress(
+                percentage,
+                minOutput,
+                maxOutput
+            );
+            return toStepSize(value, OUTPUT_STEP);
         },
-        [svgRect]
+        [outputRange, maxOutput, minOutput]
     );
 
-    // Handle dragging:
-    const onMouseMove = useCallback(
-        (e) => {
-            e.stopPropagation();
-
-            // Don't do anything if we're not dragging:
-            if (dragging === '') return;
-
-            const posY = (e.touches ? e.touches[0] : e).clientY;
-
-            // Update internal values:
-            _onChange(posY, dragging);
+    // Returns a new ratio value given a position:
+    const _positionToRatio = useCallback(
+        (posY: number) => {
+            const lowestYValue = coffeeRange[1]; // Corresponds with highest physical position!
+            const highestYValue = coffeeRange[0]; // Corresponds with lowset physical position!
+            const posWithinRange = withinRange(
+                posY,
+                lowestYValue,
+                highestYValue
+            );
+            const percentage =
+                1 -
+                rangeProgressFromValue(
+                    posWithinRange,
+                    lowestYValue,
+                    highestYValue
+                );
+            const value = rangeValueFromProgress(
+                percentage,
+                minRatio,
+                maxRatio
+            );
+            return toStepSize(value, COFFEE_STEP);
         },
-        [dragging, svgRect]
+        [coffeeRange, maxRatio, minRatio]
     );
 
-    // Handle dragging end:
-    const onMouseUp = useCallback(
-        (e) => {
-            e.stopPropagation();
-
-            // Update the state to reflect that we're no longer dragging:
-            setDragging('');
-
-            // Update external values:
-            onChange();
-        },
-        [svgRect]
-    );
-
-    // Callback to update internal output value:
+    // Is called whenever the user drags:
     const _onChange = useCallback(
         (posY: number, side: 'left' | 'right') => {
-            const percentage = _positionToPercentage(posY);
-
             if (side === 'right') {
                 // We're dragging right, update the output:
-                return setOutput(_percentageToOutput(percentage));
+                return setOutput(_positionToOutput(posY));
             } else {
                 // We're dragging left, update the ratio:
-                return setRatio(_percentageToRatio(percentage));
+                return setRatio(_positionToRatio(posY));
             }
         },
-        [svgRect]
+        [_positionToOutput, _positionToRatio]
     );
 
     // Callback to update external values:
@@ -168,9 +238,48 @@ const RatioSlider = ({
         );
     }, [ratio, output]);
 
-    const filledHeight = (output / maxOutput) * (height * (6 / 8));
-    const coffeeHeight = (ratio / 1000) * 7 * filledHeight;
-    const waterHeight = filledHeight - coffeeHeight;
+    // Handle dragging start:
+    const onMouseDown = useCallback(
+        (e) => {
+            e.stopPropagation();
+            const { x, y } = getPosition(e);
+
+            // Update state to reflect dragging state (prefer dragging right):
+            const side = x >= width / 2 ? 'right' : 'left';
+            setSide(side);
+
+            _onChange(y, side);
+        },
+        [getPosition, _onChange]
+    );
+
+    // Handle dragging:
+    const onMouseMove = useCallback(
+        (e) => {
+            e.stopPropagation();
+
+            // Don't do anything if we're not dragging:
+            if (side === '') return;
+
+            const { y } = getPosition(e);
+            _onChange(y, side);
+        },
+        [getPosition, _onChange]
+    );
+
+    // Handle dragging end:
+    const onMouseUp = useCallback(
+        (e) => {
+            e.stopPropagation();
+
+            // Update the state to reflect that we're no longer dragging:
+            setSide('');
+
+            // Update external values:
+            onChange();
+        },
+        [onChange]
+    );
 
     return (
         <svg
@@ -187,112 +296,58 @@ const RatioSlider = ({
         >
             <defs>
                 <clipPath id="clipPath">
-                    <path
-                        d={`
-                            M ${width / 4 + borderRadius},${height * (1 / 8)}
-                            h ${width / 2 - 2 * borderRadius}
-                            q ${borderRadius},0 ${borderRadius},${borderRadius}
-                            v ${height * (6 / 8) - 2 * borderRadius}
-                            q 0,${borderRadius} -${borderRadius},${borderRadius}
-                            h -${width / 2 - 2 * borderRadius}
-                            q ${-borderRadius},0 -${borderRadius},-${borderRadius}
-                            v -${height * (6 / 8) - 2 * borderRadius}
-                            q 0,-${borderRadius} ${borderRadius},-${borderRadius}
-                            z
-                        `}
-                    />
+                    <path d={clipPathPath} />
                 </clipPath>
             </defs>
             <g clipPath="url(#clipPath)" name="shape">
-                <path
-                    d={`
-                        M 0,${height * (1 / 8)}
-                        h ${width}
-                        v ${height * (6 / 8)}
-                        h -${width}
-                        v -${height * (6 / 8)}
-                        z
-                    `}
-                    fill="#E80036"
-                    name="background"
-                />
-                <g>
-                    <path
-                        d={`
-                            M 0,${height * (7 / 8) - coffeeHeight}
-                            h ${width}
-                            v -${waterHeight}
-                            h -${width}
-                            v ${waterHeight}
-                            z
-                        `}
-                        fill="white"
-                        name="output"
-                    />
-                </g>
-                <g>
-                    <path
-                        d={`
-                            M 0,${height * (7 / 8)}
-                            h ${width}
-                            v -${coffeeHeight}
-                            h -${width}
-                            v ${coffeeHeight}
-                            z
-                        `}
-                        fill="#8a0020"
-                        name="coffee"
-                    />
-                </g>
+                <path d={backgroundPath.join(' ')} fill="#E80036" name="bg" />
+                <path d={outputPath.join(' ')} fill="white" name="output" />
+                <path d={coffeePath.join(' ')} fill="#8a0020" name="coffee" />
             </g>
-            <g name="output-label">
-                <text
-                    className="text-sm capitalize"
-                    dx="10px"
-                    fill="currentColor"
-                    textAnchor="start"
-                    x={width * (3 / 4)}
-                    y={height * (7 / 8) - coffeeHeight - waterHeight / 2}
-                >
-                    Output
-                </text>
-                <text
+            <text
+                fill="currentColor"
+                name="output-label"
+                textAnchor="end"
+                {...outputLabelPosition}
+            >
+                <tspan className="text-sm capitalize">Output</tspan>
+                <tspan
                     className="font-bold text-lg"
-                    dx="10px"
+                    {...outputLabelPosition}
                     dy="1.4rem"
-                    fill="currentColor"
-                    textAnchor="start"
-                    x={width * (3 / 4)}
-                    y={height * (7 / 8) - coffeeHeight - waterHeight / 2}
                 >
                     {output}&nbsp;ml
-                </text>
-            </g>
-            <g name="coffee-label">
-                <text
-                    className="text-sm capitalize"
-                    dx="-10px"
-                    fill="currentColor"
-                    textAnchor="end"
-                    x={width / 4}
-                    y={height * (7 / 8) - coffeeHeight / 2}
-                >
-                    Coffee
-                </text>
-                <text
+                </tspan>
+            </text>
+            <text
+                fill="currentColor"
+                name="coffee-label"
+                textAnchor="end"
+                {...coffeeLabelPosition}
+            >
+                <tspan className="text-sm capitalize">Coffee</tspan>
+                <tspan
                     className="font-bold text-lg"
-                    dx="-10px"
+                    {...coffeeLabelPosition}
                     dy="1.4rem"
-                    fill="currentColor"
-                    textAnchor="end"
-                    x={width / 4}
-                    y={height * (7 / 8) - coffeeHeight / 2}
                 >
                     {round((ratio / 1000) * output)}&nbsp;g
-                </text>
-            </g>
+                </tspan>
+            </text>
         </svg>
     );
 };
+
+const withinRange = (val: number, min: number, max: number) =>
+    Math.min(Math.max(val, min), max);
+
+const toStepSize = (val: number, stepSize: number) =>
+    Math.ceil(val / stepSize) * stepSize;
+
+const rangeProgressFromValue = (val: number, min: number, max: number) =>
+    withinRange((val - min) / (max - min), 0, 1);
+
+const rangeValueFromProgress = (progress: number, min: number, max: number) =>
+    min + progress * (max - min);
 
 export default RatioSlider;
